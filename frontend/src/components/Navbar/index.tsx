ca6ad601b5bce0d1fd4588df10b9d0415ffbd221
iago <35708_iago@callink.com.br>
2022-05-16 16:26:42 -0300

    navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return (
+    <header>
+    <nav className="container">
+        <div className="webmovie-nav-content">
+            <h1>WebMovie</h1>
+            <a href="https://github.com/IagoSilvaSantos" target="_blank" rel="noreferrer">
+                <div className="webmovie-contact-container">
+                    <GithubIcon />
+                    <p className="webmovie-contact-link">/Git Hub Repository</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
+    );
+}
+
+export default Navbar;
