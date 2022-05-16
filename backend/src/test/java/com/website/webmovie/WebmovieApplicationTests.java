5c4c9ad91811474e5e4ff5deb58ec232534967c7
iago <35708_iago@callink.com.br>
2022-05-16 14:48:55 -0300

    Project created

---
+++ backend/src/test/java/com/website/webmovie/WebmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.webmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
