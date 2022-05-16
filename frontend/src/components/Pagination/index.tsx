2d8554c31e79d6acd1fed0d50109d0d45ec7bfde
iago <35708_iago@callink.com.br>
2022-05-16 18:20:27 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -4,14 +4,14 @@ import './styles.css';
 
 function Pagination() {
   return (
-    <div className="dsmovie-pagination-container">
-      <div className="dsmovie-pagination-box">
-        <button className="dsmovie-pagination-button" disabled={true}>
+    <div className="webmovie-pagination-container">
+      <div className="webmovie-pagination-box">
+        <button className="webmovie-pagination-button" disabled={true}>
           <Arrow />
         </button>
         <p>{`${1} de ${3}`}</p>
-        <button className="dsmovie-pagination-button" disabled={false}>
-          <Arrow className="dsmovie-flip-horizontal" />
+        <button className="webmovie-pagination-button" disabled={false}>
+          <Arrow className="webmovie-flip-horizontal" />
         </button>
       </div>
     </div>
