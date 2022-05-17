366cbf5dc2892847d3c85032019e3e6ad5f5389c
iago <35708_iago@callink.com.br>
2022-05-17 14:57:13 -0300

    show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -19,7 +19,7 @@ function MovieCard( { movie }: Props) {
       />
       <div className="webmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score}/>
         <Link to={`/form/${movie.id}`}>
         <div className="btn btn-primary webmovie-btn">Avaliar</div>
         </Link>
